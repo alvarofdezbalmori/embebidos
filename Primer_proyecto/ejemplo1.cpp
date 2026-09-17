27b6503afae71ab584a35d42f669f250c225434f
Personal <Personal@LAPTOP-VA199JJC>
2026-09-17 18:23:37 +0200

    Primer desarrollo

---
+++ Primer_proyecto/ejemplo1.cpp
@@ -0,0 +1,28 @@
+/*
+ * ejemplo1.cpp
+ *
+ *  Created on: 17 sept 2026
+ *      Author: Personal
+ */
+
+#include <iostream>
+
+int suma(int d1, int d2){
+
+	return d1+d2;
+}
+
+
+int main(int narg, char *arg[]){
+
+	int d1=50;
+	int d2=50;
+	if(narg>1){
+		d1=std::atoi(arg[1]);
+	}
+	if(narg>1){
+		d2=std::atoi(arg[2]);
+	}
+
+	std::cout << "suma"<<d1<<"y"<<d2<<"="<<suma(d1,d2)<<std::endl;
+}
